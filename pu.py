73bf310aeafbb4d1faf11f082a9c73de87fe3504
Iliya Puzhaev <Puzhaev@MacBook-Pro-Iliya.local>
2017-05-30 01:41:24 +0300

    “norm”

--- pu.py
+++ pu.py
@@ -1,18 +1,18 @@
-import json
+import bs4
 import  requests
 import sqlite3
 import urllib2
 
 conn = sqlite3.connect('Chinook_Sqlite.sqlite')
 cursor = conn.cursor()
-cursor.execute( '''CREATE TABLE inf(name auto_increment primary key, data, photo,author)''')
+
 
 class Novosti:
     name = ''
     data = ''
     photo = ''
     author = ''
-
+    
     def __init__(self,name , data, photo,author):
         self.name = name
         self.data = data
@@ -20,17 +20,15 @@ class Novosti:
         self.author = author
 
 
-def  inf  ( url ):
+def  info  ( url ):
     data  = urllib2.urlopen(url).read()
     soup = bs4.BeautifulSoup(data, 'html.parser')
+    
+    c = Novosti( soup.title.text  ,soup.find('span', {'class': "article-content__data-date"}).text , soup.find(property="og:image").text , soup.find('a', {'class': "article-content__data-authors"}).text    )
+    cursor.execute(" insert into inf values ('%s', '%s', '%s', '%s');" %(answer.name, answer.data, answer.photo, answer.author))
 
-    c = Novosti( soup.title , , ,  )
-    cursor.execute(" insert into information values (%s, '%s', '%s', '%s');" %(answer.name, answer.data, answer.photo, answer.author)))
-
-l = ''
 for i in range(10):
-    l.append(input())
-    def(l)
+    info(input())
 
 
 
